c30fc2d3aa5db6a491a094b4395d9ea3487c3002
Alessio <alessio.mazzella@siren.io>
2023-09-27 15:00:00 +0200

    fizzbuzz-array completed

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,23 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+  const finalArray = [];
+
+  for (let i = 1; i <= num; i++) {
+    switch (true) {
+      case i % 15 === 0:
+        finalArray.push('FizzBuzz');
+        break;
+      case i % 3 === 0:
+        finalArray.push('Fizz');
+        break;
+      case i % 5 === 0:
+        finalArray.push('Buzz');
+        break;
+      default:
+        finalArray.push(i);
+    }
+  }
+
+  return finalArray;
+}
 
 module.exports = fizzBuzzArray;
