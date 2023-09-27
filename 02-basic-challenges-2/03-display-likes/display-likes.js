1d26bb7245a014df7bf996f4035835f5a4206733
Alessio <alessio.mazzella@siren.io>
2023-09-27 16:23:22 +0200

    display-likes completed

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,23 @@
-function displayLikes() {}
+function displayLikes(arrNames) {
+  switch (arrNames.length) {
+    case 0: {
+      return 'no one likes this';
+    }
+    case 1: {
+      return `${arrNames[0]} likes this`;
+    }
+    case 2: {
+      return `${arrNames[0]} and ${arrNames[1]} like this`;
+    }
+    case 3: {
+      return `${arrNames[0]}, ${arrNames[1]} and ${arrNames[2]} like this`;
+    }
+    default: {
+      return `${arrNames[0]}, ${arrNames[1]} and ${
+        arrNames.length - 2
+      } others like this`;
+    }
+  }
+}
 
 module.exports = displayLikes;
