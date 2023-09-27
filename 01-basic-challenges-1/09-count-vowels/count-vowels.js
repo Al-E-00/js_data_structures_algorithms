e855d41a5c8120950b18a25cf25046b511b3dbe2
Alessio <alessio.mazzella@siren.io>
2023-09-27 09:59:51 +0200

    count-vowels passed

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,20 @@
-function countVowels() {}
+const __VOWELS = ['a', 'e', 'i', 'o', 'u'];
+
+function countVowels(str) {
+  const stringLength = str.length;
+  const stringLowerCase = str.toLowerCase();
+
+  const letterInStringArray = stringLowerCase.split('');
+
+  let vowelsCount = 0;
+
+  for (let i = 0; i < stringLength; i++) {
+    if (__VOWELS.includes(letterInStringArray[i])) {
+      vowelsCount++;
+    }
+  }
+
+  return vowelsCount;
+}
 
 module.exports = countVowels;
