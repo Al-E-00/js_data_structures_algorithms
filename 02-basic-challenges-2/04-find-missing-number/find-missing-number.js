a3d5b310ea1da659e14482aef677fd1f8be85d84
Alessio <alessio.mazzella@siren.io>
2023-09-27 16:35:37 +0200

    find-missing-number completed

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,14 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+    if(!arr) return undefined;
+
+    if(arr.length === 0) return 1;
+
+
+    for(let i = 1; i <= arr.length; i++) {
+        if(!arr.includes(i)) {
+            return i
+        }
+    }
+}
 
 module.exports = findMissingNumber;
