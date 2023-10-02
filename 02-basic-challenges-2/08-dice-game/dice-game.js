12810454308e0587eb98daa0c818e2aa28ce8a2e
Alessio <alessio.mazzella@siren.io>
2023-10-02 17:17:12 +0200

    dice-game-run completed

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,42 @@
-function diceGameSimulation() {}
+function diceGameSimulation(numSimulations) {
+  const finalResult = []
 
-module.exports = diceGameSimulation;
+  for (let i = 0; i < numSimulations; i++) {
+    randomNum1 = rollDice()
+    randomNum2 = rollDice()
+    const sum = randomNum1 + randomNum2
+    let result = ""
+
+    switch (sum) {
+      case 7:
+      case 11: {
+        result = "win"
+        break
+      }
+      case 2:
+      case 3:
+      case 12: {
+        result = "lose"
+        break
+      }
+      default: {
+        result = "roll again"
+      }
+    }
+
+    finalResult.push({
+      dice1: randomNum1,
+      dice2: randomNum2,
+      sum: sum,
+      result: result,
+    })
+  }
+
+  function rollDice() {
+    return Math.floor(Math.random() * 6) + 1
+  }
+
+  return finalResult
+}
+
+module.exports = diceGameSimulation
