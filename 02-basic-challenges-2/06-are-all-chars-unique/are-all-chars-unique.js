b8b908cdc36bc14e4000a832f8b1928dc2c104f0
Alessio <alessio.mazzella@siren.io>
2023-10-02 15:55:50 +0200

    Are all chars unique completed

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,29 @@
-function areAllCharactersUnique() {}
+const areAllCharactersUnique = (str) => {
+  if (str.length === 0) return true
 
-module.exports = areAllCharactersUnique;
+  const arrFromString = str.split("")
+  const arrFromSet = Array.from(new Set(arrFromString))
+
+  if (arrFromString.length > arrFromSet.length) {
+    return false
+  }
+  return true
+}
+
+/* const areAllCharactersUnique = (str) => {
+  const charSet = new Set()
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i]
+
+    if (charSet.has(char)) {
+      return false
+    }
+
+    charSet.add(char)
+  }
+
+  return true
+} */
+
+module.exports = areAllCharactersUnique
