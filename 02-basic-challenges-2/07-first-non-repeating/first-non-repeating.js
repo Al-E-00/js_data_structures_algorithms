a78d7488ee2c9dcc0e0a500d7dfd189266e540b0
Alessio <alessio.mazzella@siren.io>
2023-10-02 16:39:19 +0200

    first non repeating completed

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,49 @@
-function findFirstNonRepeatingCharacter() {}
+const findFirstNonRepeatingCharacter = (str) => {
+  const strArr = str.split("")
 
-module.exports = findFirstNonRepeatingCharacter;
+  for (let i = 0; i < str.length; i++) {
+    const filteredArr = strArr.filter((string) => string === str[i])
+
+    if (filteredArr.length === 1) {
+      return filteredArr[0]
+    }
+  }
+
+  return null
+}
+
+//WITH {}
+/* const findFirstNonRepeatingCharacter = (str) => {
+  const strChar = {}
+
+  for (const char of str) {
+    strChar[char] = (strChar[char] || 0) + 1
+  }
+
+  for (const char of str) {
+    if (strChar[char] === 1) {
+      return char
+    }
+  }
+
+  return null
+} */
+
+//WITH Map
+/* const findFirstNonRepeatingCharacter = (str) => {
+  let strCharArr = new Map()
+
+  for (const char of str) {
+    strCharArr.set(char, (strCharArr.get(char) || 0) + 1)
+  }
+
+  for (const char of str) {
+    if (strCharArr.get(char) === 1) {
+      return char
+    }
+  }
+
+  return null
+} */
+
+module.exports = findFirstNonRepeatingCharacter
