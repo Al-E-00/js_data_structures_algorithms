d3c268bcc39543371268fd6e428d583c7fd07a0f
Alessio <alessio.mazzella@siren.io>
2023-10-02 17:35:13 +0200

    format-phone-number completed

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,21 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(numbersArr) {
+    const areaCode = numbersArr.slice(0, 3).join('');
+    const prefix = numbersArr.slice(3, 6).join('');
+    const lineNumber = numbersArr.slice(6).join('');
 
-module.exports = formatPhoneNumber;
+    return `(${areaCode}) ${prefix}-${lineNumber}`
+}
+
+/* function formatPhoneNumber(numbersArr) {
+   const formatted = numbersArr.join('');
+
+    return `(${formatted.substring(0, 3)}) ${formatted.substring(3, 6)}-${formatted.substring(6)}`
+}
+ */
+
+/* const formatPhoneNumber = (numbersArr) =>
+  `(${numbersArr.slice(0, 3).join("")}) ${numbersArr
+    .slice(3, 6)
+    .join("")}-${numbersArr.slice(6).join("")}` */
+
+module.exports = formatPhoneNumber
