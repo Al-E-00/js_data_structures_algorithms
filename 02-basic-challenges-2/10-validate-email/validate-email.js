0de2032a87faddfbc275806317106b10b1cc982e
Alessio <alessio.mazzella@siren.io>
2023-10-03 17:40:39 +0200

    validate-email-address completed

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,27 @@
-function validateEmail() {}
+function validateEmail(string) {
+  const [localPart, domain] = string.split("@")
 
-module.exports = validateEmail;
+  if ((localPart?.trim().length > 3) && (domain?.trim().length > 2)) {
+    if (string.indexOf("@") !== -1 && string.indexOf(".") !== -1) {
+      return true
+    }
+    return false
+  }
+  return false
+}
+
+/* const validateEmail = (string) => {
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+  return emailRegex.test(string)
+} */
+
+/* const validateEmail = (string) => {
+    if(string.indexOf('@') === -1 || string.indexOf('.') === -1) {
+        return false
+    }
+
+    return true;
+} */
+
+module.exports = validateEmail
