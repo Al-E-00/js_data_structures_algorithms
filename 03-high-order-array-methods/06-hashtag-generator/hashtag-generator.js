9dcc98b2e9372d789b4957ef86efde3b5893523d
Alessio <alessio.mazzella@siren.io>
2023-10-06 21:06:15 +0200

    hastag-generator completed

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,22 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+  if (str.trim().length === 0 || str.length > 140) return false
 
-module.exports = generateHashtag;
+  const wordsInStr = str.split(/\s+/)
+
+  const concatWords = wordsInStr.map(
+    (word) => word.charAt(0).toUpperCase() + word.substr(1)
+  )
+
+  return `#${concatWords.join("")}`
+}
+
+/* const generateHashtag = (str) => {
+    if (str.trim().length === 0 || str.length > 140) return false
+    const hastagReadyWords = str.split(/\s+/).reduce((acc, word) => {
+        return acc + (word.charAt(0).toUpperCase() + word.substr(1))
+    }, '#')
+
+    return hastagReadyWords
+} */
+
+module.exports = generateHashtag
