6822d1477839c0d3c55913f279ed19a6399dfe09
Alessio <alessio.mazzella@siren.io>
2023-10-06 15:25:38 +0200

    calculate-total-sales completed

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,29 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxRate) {
+  const totalRevenueArray = products.map(
+    (product) => product.price * product.quantity
+  )
 
-module.exports = calculateTotalSalesWithTax;
+  const totalRevenue = totalRevenueArray.reduce(
+    (total, revenue) => total + revenue,
+    0
+  )
+
+  const taxesAmount = (totalRevenue * taxRate) / 100
+  const totalAmount = totalRevenue + taxesAmount
+  return +totalAmount.toFixed(2)
+}
+
+/* const calculateTotalSalesWithTax = (products, taxRate) => {
+  const partialAmount = products.reduce(
+    (total, product) => total + product.price * product.quantity,
+    0
+  )
+
+  const taxesAmount = (partialAmount * taxRate) / 100
+
+  const totalAmount = +(partialAmount + taxesAmount).toFixed(2)
+
+  return totalAmount
+}
+ */
+module.exports = calculateTotalSalesWithTax
