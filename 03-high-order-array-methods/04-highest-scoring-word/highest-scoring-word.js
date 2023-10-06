4a7078619123ce06ec30f99fbb77c2f468e1ae17
Alessio <alessio.mazzella@siren.io>
2023-10-06 16:05:54 +0200

    highest-scoring-word completed

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,22 @@
-function highestScoringWord() {}
+function highestScoringWord(str) {
+  const wordsInStr = str.split(" ")
+  const wordScoreArray = []
 
-module.exports = highestScoringWord;
+  wordsInStr.forEach((word) => {
+    const singleLetterArray = word.split("")
+
+    const wordTotalScore = singleLetterArray.reduce(
+      (total, letter) => total + letter.charCodeAt(0),
+      0
+    )
+
+    wordScoreArray.push(wordTotalScore)
+  })
+
+  const highestWordScore = wordScoreArray.sort()[wordScoreArray.length - 1];
+  const indexOfTheWinnerWord = wordScoreArray.findIndex(num => num === highestWordScore);
+
+  return wordsInStr[indexOfTheWinnerWord];
+}
+
+module.exports = highestScoringWord
