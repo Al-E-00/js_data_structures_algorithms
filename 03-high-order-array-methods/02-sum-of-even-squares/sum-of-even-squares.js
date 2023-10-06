a90cb8caa80f6729d18a3dce4a6be7608fdcc738
Alessio <alessio.mazzella@siren.io>
2023-10-06 14:47:32 +0200

    sum-of-even-squares completed

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,18 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numbers) {
+  if (numbers.length === 0) return 0
 
-module.exports = sumOfEvenSquares;
+  const evenNumber = numbers.filter((num) => num % 2 === 0)
+  const sumOfSquares = evenNumber.reduce((tot, num) => tot + num ** 2, 0)
+  return sumOfSquares
+}
+
+/* const sumOfEvenSquares = (numbers) => {
+    const evenSquares = numbers
+        .filter(num % 2 === 0)
+        .map(num => num ** 2)
+        .reduce((num, total) => total + num, 0);
+
+    return evenSquares;
+} */
+
+module.exports = sumOfEvenSquares
